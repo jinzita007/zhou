25aa667fcc8867e84002cab434200ee4ec5b6114
zhou <jinzita007@yahoo.com>
2016-04-13 11:03:19 +0800

    1.1

---
+++ 1.1/js/animate.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#owl-demo').owlCarousel({
+        items: 5,
+        lazyLoad: true,
+        /*itemsDesktop: true,
+        itemsDesktopSmall: true,*/
+        itemsTablet:true,
+        itemsTabletSmall:false,
+        itemsMobile:true,
+
+    });
+});
+
